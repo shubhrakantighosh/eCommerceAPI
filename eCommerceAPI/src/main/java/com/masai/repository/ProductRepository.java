7f6565adc4c2d81ed60b738873e9c885344a275c
shubhrakantighosh <shubhrakantighosh1996@gmail.com>
2022-12-01 23:51:54 +0530

    update

--- eCommerceAPI/src/main/java/com/masai/repository/ProductRepository.java
+++ eCommerceAPI/src/main/java/com/masai/repository/ProductRepository.java
@@ -14,4 +14,12 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
     @Query(" select new com.masai.model.ProductDTO (product.productId ,product.productName, product.productPrice) from Product product ")
     List<ProductDTO> products();
 
+    @Query(" select new com.masai.model.ProductDTO (product.productId ,product.productName, product.productPrice) from Product product where product.category.categoryId=?1 ")
+    List<ProductDTO> productsSearchByCategoryId(Integer categoryId);
+
+    @Query(" select new com.masai.model.ProductDTO (product.productId ,product.productName, product.productPrice) from Product product where product.productPrice>=?1 AND product.productPrice<=?2")
+    List<ProductDTO> searchByProductPrice(Double minprice,Double maxprice);
+
+    @Query("select new com.masai.model.ProductDTO(product.productId,product.productName,product.productPrice) from Product product where product.productName=?1")
+    ProductDTO viewProductByName(String name);
 }
